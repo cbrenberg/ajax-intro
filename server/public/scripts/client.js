846c82705b2478e7406f20535916f4259ece0ff1
Christopher Brenberg <cbrenberg@gmail.com>
2018-09-06 11:38:41 -0500

    allow books to be added and displayed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,30 @@ console.log('client.js');
 
 $(document).ready(readyNow);
 
+function addBook() {
+  console.log('in addBook');
+  //get user inputs & create new object
+  let objectToSend = {
+    title: $('#titleIn').val(),
+    author: $('#authorIn').val(),
+    genre: $('#genreIn').val(),
+    pages: $('#pagesIn').val()
+  }
+  console.log(objectToSend)
+  //send object to the server via AJAX POST
+  $.ajax({
+    method: 'POST',
+    url: '/books',
+    data: objectToSend
+  }).then(function(response){
+    console.log('back from server with:', response);
+    //update books display
+    getBooksFromServer();
+  }).catch(function(error){
+    alert('error:', error)
+  })
+}
+
 function getBooksFromServer() {
   $.ajax({
     method: 'GET',
@@ -9,6 +33,7 @@ function getBooksFromServer() {
   }).then( function( response ) {
     console.log('back from server with:', response);
     let el = $('#booksOut');
+    el.empty();
     for (let book of response) {
       el.append(`<li>${book.title} by ${book.author}</li>`);
     }
@@ -18,6 +43,6 @@ function getBooksFromServer() {
 } //end getBooksFromServer
 
 function readyNow () {
-  console.log('jQuery');
+  $('#addBookButton').on('click', addBook);
   getBooksFromServer();
 } // end readyNow
